90a7b9b295e0f91f11c87e1654af99948a038044
Diana <ganeeva.d.f@gmail.com>
2020-02-15 14:05:19 +0300

    empty project

---
+++ TimeManagement/app/src/main/java/com/ganeeva/d/f/timemanagement/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ganeeva.d.f.timemanagement
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
